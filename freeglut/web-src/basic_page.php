2d4c487afffeee27e4f328948f7db7fd23220e57
rkrolib <rkrolib@7f0cb862-5218-0410-a997-914c9d46530a>
2003-12-25 16:22:03 +0000

    Some cleanup and touchup to the web-pages.
    Nothing exciting.
    
    
    git-svn-id: https://svn.code.sf.net/p/freeglut/code/trunk@416 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/web-src/basic_page.php
+++ freeglut/web-src/basic_page.php
@@ -14,7 +14,7 @@ generateHeader($_SERVER['PHP_SELF']);
 ?>
 
 <div class="textheader">What?</div>
-<p>freeglut is a completely OpenSourced alternative to the OpenGL Utility Toolkit (GLUT) library. GLUT was originally written by Mark Kilgard to support the sample programs in the second edition OpenGL 'RedBook'. Since then, GLUT has been used in a wide variety of practical applications because it is simple, universally available and highly portable.</p>
+<p>freeglut is a completely OpenSourced alternative to the OpenGL Utility Toolkit (GLUT) library. GLUT was originally written by Mark Kilgard to support the sample programs in the second edition OpenGL 'RedBook'. Since then, GLUT has been used in a wide variety of practical applications because it is simple, widely available and highly portable.</p>
 
 
 <?PHP
--- freeglut/web-src/docs/install.php
+++ freeglut/web-src/docs/install.php
@@ -16,8 +16,12 @@ generateHeader($_SERVER['PHP_SELF']);
 <div class="textheader">How to install freeglut...</div>
 <p>It's not *that* hard...</p>
 
+<p>Presently, there are few pre-packaged installs, so the recommended method for most users is to follow these instructions.  Even if you have a package system that includes freeglut, you may wish to manually install for a variety of reasons.  A likely reason is simply that the latest version of freeglut may not have been packaged.</p>
+
+<p>So...</p>
+
 <div class="textheader">Download freeglut</div>
-<p>First, you need to download a freeglut release. Newer is generally better, but unless you know what you're doing, you should use a release marked "Stable." So download the newest Stable release that you can.</p>
+<p>First, you need to download a freeglut release. Newer is generally better, but unless you know what you're doing, you should use a release marked "Stable." So download the newest Stable release that you can.  (On the other hand, if you discover a bug and want to report it, it is helpful to try to run the "current" freeglut, as we have been known to fix some bugs before they are reported.)</p>
 
 <div class="textheader">Untar and configure the packages</div>
 <p>Copy the tarball you downloaded into a temporary directory. Untar it as follows:</p>
@@ -28,6 +32,12 @@ generateHeader($_SERVER['PHP_SELF']);
  
 <div class="textheader">Now, configure the packages:</div>
 <blockquote>./configure</blockquote>
+
+<p>NetBSD users may need a slightly different command:</p>
+
+<blockquote><tt>LDFLAGS=-Wl,-R/usr/X11R6/lib ./configure</tt></blockquote>
+
+<p>...to properly handle things.</p>
 
 <div class="textheader">Build and install freeglut</div>
 <p>Once that completes, it's time to build and install freeglut (Note that you will have to be the superuser to make install).</p>
