83ef66f3989990d3bc4fd1e2140eb10d79709ea6
beuc <beuc@7f0cb862-5218-0410-a997-914c9d46530a>
2012-04-01 19:59:28 +0000

    Start page about Android
    git-svn-id: https://svn.code.sf.net/p/freeglut/code/trunk@1229 7f0cb862-5218-0410-a997-914c9d46530a

---
+++ freeglut/web-src/docs/android.php
@@ -0,0 +1,70 @@
+<?php
+require("../template.php");
+
+# Now set the title of the page:
+setPageTitle("Android");
+
+# Make the header.
+generateHeader($_SERVER['PHP_SELF']);
+?>
+
+<ul>
+<li><a href="#roadmap">Roadmap</a></li>
+<li><a href="#api">New API</a></li>
+<li><a href="#compiling">Compiling</a></li>
+<li><a href="#using">Using in your projects</a></li>
+</ul>
+
+
+<a name="roadmap"></a>
+<h1>Roadmap</h1>
+
+Done:
+<ul>
+<li>Initialize context with EGL</li>
+<li>Keyboard support</li>
+<li>Mouse support</li>
+<li>Virtual keypad (on touchscreen)</li>
+<li>Extract assets in cache dir on start-up</li>
+<li>Make EGL support reusable by Mesa X11</li>
+<li>freeglut_std.h can be used with GLES1 or GLES2 or non-ES headers<br />
+(using -DFREEGLUT_GLES1 and -DFREEGLUT_GLES2)</li>
+</ul>
+
+In progress:
+<ul>
+<li>GLES2 support for geometry, menus and basic fonts
+(possibly also GLES1)</li>
+</ul>
+
+TODO:
+<ul>
+<li>Open new windows (if that's possible)</li>
+<li>Cursor support</li>
+<li>Joystick support (xperia play...)</li>
+<li>Display translucent keys on virtual keypad</li>
+<li>API to detect touchscreen presence</li>
+<li>API to disable assets extraction</li>
+</ul>
+
+<a name="api"></a>
+<h1>New API</h1>
+
+New functions will be necessary to :
+<ul>
+<li>detect touchscreen presence</li>
+<li>disable assets extraction</li>
+</ul>
+
+(Work In Progress)
+
+<a name="compiling"></a>
+<h1>Compiling</h1>
+
+<h2>Create a module compatible with the NDK build-system</h2>
+<h2>Compile FreeGLUT for a traditional cross-compiler environment</h2>
+
+<a name="using"></a>
+<h1>Using in your projects</h1>
+
+<?php generateFooter(); ?>
