1bdc52ff5dd05a4a89aac175af6582f437ba7447
dcnieho <dcnieho@7f0cb862-5218-0410-a997-914c9d46530a>
2013-04-05 03:36:16 +0000

    added 3.0 todo list and call for help
    git-svn-id: https://svn.code.sf.net/p/freeglut/code/trunk@1592 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/web-src/help.php
+++ freeglut/web-src/help.php
@@ -7,26 +7,25 @@ setPageTitle("How You Can Help");
 # Make the header.
 generateHeader($_SERVER['PHP_SELF']);
 ?>
-
-<p></p>
-
 <div class="img-right">
 <img src="images/OGL.png" alt="OpenGL" /><br/>
 This is OpenGL at its finest.
 </div>
 
 <div class="textheader">This is better than GLUT.</div>
-<p>freeglut started as a GLUT clone, but now it's something much better than that. It's a GLUT replacement. Here are the features you have wanted -- and if you have ideas for more, share them!</p> 
+<p>FreeGLUT started out as a GLUT clone, but now it's something much
+better than that. It's a GLUT replacement. Here are the features you
+have wanted -- and if you have ideas for more, share them! FreeGLUT is
+under continuous development. Currently, work on 3.0 is in full swing.
+See below for the list of work that is still to be done.</p> 
 
 <div class="textheader">Everyone can help</div>
 <p>
 Free software projects depend on the community around them for their growth. 
 It's by contributions that truly wonderful pieces of open and free software come to be.
-</p> 
+Contributions can be nearly anything.  The point is involvement!</p>
 
-<p>Contributions can be nearly anything.  The point is involvement!</p>
-
-<p>Examples:</p>
+<p>Examples:
 <ul>
 	<li>Use freeglut in your projects!</li>
 	<li>Tell us what you like.</li>
@@ -36,11 +35,19 @@ It's by contributions that truly wonderful pieces of open and free software come
 	<li>Spread the word about the project.</li>
 	<li><a href="http://lists.sourceforge.net/lists/listinfo/freeglut-developer">Join</a> our development mailing list.</li>
 	<li>If you changed a feature, send us a patch -- others might benefit from your work!</li>
-</ul>
+</ul></p>
 
 <p>
 But simply, become involved!
 The project is not just software, it's the people that contribute, too.
+</p>
+
+<div class="textheader">FreeGLUT 3.0</div>
+<p>
+<a href=progress.php>Click here</a> for an overview of the major work,
+its status and planned milestone for FreeGLUT 3.0 and beyond. Help one
+any of these plans, as well as suggestions and/or patches for anything
+else are very welcome!
 </p>
 
 <div class="textheader">Go to SourceForge and...</div>
