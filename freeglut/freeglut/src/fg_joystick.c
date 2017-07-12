b39b3a5edf2f1636d2d7c687f9163f2b0045ee18
dcnieho <dcnieho@7f0cb862-5218-0410-a997-914c9d46530a>
2017-07-12 20:44:35 +0000

    Fix build on NetBSD.
    (cherry picked from commit 06e1d815cf9abda49b845754ff420734446eb95b)
    
    (cherry picked from commit c4294ad59093c27159ea6028d33e3c9581df5354)
    
    git-svn-id: https://svn.code.sf.net/p/freeglut/code/trunk@1829 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/freeglut/src/fg_joystick.c
+++ freeglut/freeglut/src/fg_joystick.c
@@ -46,21 +46,20 @@
 
 #    ifdef HAVE_USB_JS
 #        if defined(__NetBSD__)
-/* XXX The below hack is done until freeglut's autoconf is updated. */
-#            define HAVE_USBHID_H 1
 #            ifdef HAVE_USBHID_H
 #                include <usbhid.h>
 #            else
 #                include <usb.h>
 #            endif
+#            include <dev/usb/usb.h>
 #        elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 #            ifdef HAVE_USBHID_H
 #                include <usbhid.h>
 #            else
 #                include <libusbhid.h>
 #            endif
+#            include <legacy/dev/usb/usb.h>
 #        endif
-#        include <legacy/dev/usb/usb.h>
 #        include <dev/usb/usbhid.h>
 
 /* Compatibility with older usb.h revisions */
