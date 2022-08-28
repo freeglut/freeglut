beef7e20bd2f96c72a920bafa6c896fd548db36b
John Tsiombikas <nuclear@member.fsf.org>
2022-08-28 16:43:24 +0300

    need time.h for nanosleep

--- src/x11/fg_gamemode_x11.c
+++ src/x11/fg_gamemode_x11.c
@@ -25,7 +25,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
+#include <time.h>
 #include <GL/freeglut.h>
 #include "../fg_internal.h"
 
