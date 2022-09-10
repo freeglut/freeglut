82479766b69c71fe3aeaca9da0dc626e6b51745c
Jonatha Gabriel <jonathagabrielns@gmail.com>
2022-09-10 09:15:17 -0300

    Silents warning about discarding 'const' qualifier

--- src/fg_font.c
+++ src/fg_font.c
@@ -59,19 +59,19 @@ extern const SFG_StrokeFont fgStrokeMonoRoman;
 SFG_Font* fghFontByID( void* font )
 {
     if( font == GLUT_BITMAP_8_BY_13        )
-        return &fgFontFixed8x13;
+        return (SFG_Font*) &fgFontFixed8x13;
     if( font == GLUT_BITMAP_9_BY_15        )
-        return &fgFontFixed9x15;
+        return (SFG_Font*) &fgFontFixed9x15;
     if( font == GLUT_BITMAP_HELVETICA_10   )
-        return &fgFontHelvetica10;
+        return (SFG_Font*) &fgFontHelvetica10;
     if( font == GLUT_BITMAP_HELVETICA_12   )
-        return &fgFontHelvetica12;
+        return (SFG_Font*) &fgFontHelvetica12;
     if( font == GLUT_BITMAP_HELVETICA_18   )
-        return &fgFontHelvetica18;
+        return (SFG_Font*) &fgFontHelvetica18;
     if( font == GLUT_BITMAP_TIMES_ROMAN_10 )
-        return &fgFontTimesRoman10;
+        return (SFG_Font*) &fgFontTimesRoman10;
     if( font == GLUT_BITMAP_TIMES_ROMAN_24 )
-        return &fgFontTimesRoman24;
+        return (SFG_Font*) &fgFontTimesRoman24;
 
     return 0;
 }
@@ -83,9 +83,9 @@ SFG_Font* fghFontByID( void* font )
 static SFG_StrokeFont* fghStrokeByID( void* font )
 {
     if( font == GLUT_STROKE_ROMAN      )
-        return &fgStrokeRoman;
+        return (SFG_StrokeFont*) &fgStrokeRoman;
     if( font == GLUT_STROKE_MONO_ROMAN )
-        return &fgStrokeMonoRoman;
+        return (SFG_StrokeFont*) &fgStrokeMonoRoman;
 
     return 0;
 }
